c4305d8103e424b9349331e390422c92ac53c858
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-08-29 19:01:37 -0500

    Add Github & Website Links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        Coded by <a href="https://carreauxcreativ.com" target="_blank">Julicia</a> | Open-sourced on <a href="https://github.com/LeSteez/react-weather-app" target="_blank">Github</a>
+      </footer>
     </div>
   );
 }
