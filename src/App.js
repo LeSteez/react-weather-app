7ab3c94bb71299268de6e86fbb3ad3b0be8e5753
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-08-29 19:18:34 -0500

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         Coded by <a href="https://carreauxcreativ.com" target="_blank">Julicia</a> | Open-sourced on <a href="https://github.com/LeSteez/react-weather-app" target="_blank">Github</a>
       </footer>
+      </div>
     </div>
   );
 }
