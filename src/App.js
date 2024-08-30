30d286725b1e5ca2c62a19b746fd9483c6a70d10
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-08-29 23:01:47 -0500

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-
 import './App.css';
 import Weather from './Weather';
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        Coded by <a href="https://carreauxcreativ.com" target="_blank">Julicia</a> | Open-sourced on <a href="https://github.com/LeSteez/react-weather-app" target="_blank">Github</a>
-      </footer>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>Weather App</h1>
+                <Weather/>
+                <footer>
+                    Coded by <a href="https://carreauxcreativ.com"
+                                target="_blank">Julicia</a> | Open-sourced on
+                    <a
+                    href="https://github.com/LeSteez/react-weather-app"
+                    target="_blank">Github</a>
+                </footer>
+            </div>
+        </div>
+    );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,11 +11,12 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
 
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input type="submit" value="Search" className="btn btn-primary w-100" />
           </div>
         </div>
       </form>
@@ -26,15 +27,20 @@ export default function Weather() {
         <li>Sunny</li>
       </ul>
 
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
+            <div className="clearfix">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt="Sunny"
-          ></img>
-          88°F
+            className="float-left"
+          />
+                <div className="float-left">
+            <span className="temperature">88</span>
+            <span className="unit">°F</span>
+                </div>
         </div>
-
+        </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
