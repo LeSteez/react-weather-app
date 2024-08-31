a5b7e90e313b6483bfd6ed7f1afffeab12535704
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-08-31 11:43:24 -0500

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -11,8 +11,6 @@ export default function FormattedDate(props) {
         'Saturday'
     ];
     let day = days[props.date.getDay()];
-    let hours = props.date.getHours();
-    let minutes = props.date.getMinutes();
     let currentTime = new Date();
     let options = {timeStyle: 'short', hour12: true};
     let timeString = currentTime.toLocaleTimeString('en-US', options);
