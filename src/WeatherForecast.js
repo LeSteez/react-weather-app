bb5b1f2c391ce886d9a9cb3f46c1917e51e41635
LeSteez <146269402+LeSteez@users.noreply.github.com>
2024-09-01 02:28:49 -0500

    removed daily forecast loop

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,15 +15,9 @@ export default function WeatherForecast(props) {
         return (
             <div className="WeatherForecast">
                 <div className="row">
-                    {daily.map(function(dailyForecast, index) {
-                        if (index < 5) {
-                            return (
-                                <div className="col" key={index}>
-                                    <WeatherForecastDay data={dailyForecast} />
-                                </div>
-                            );
-                        }
-                    })}
+                    <div className="col">
+                        <WeatherForecastDay data={daily[0]} />
+                    </div>
                 </div>
             </div>
         );
